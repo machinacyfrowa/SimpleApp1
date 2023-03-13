dcd6c025afd0d92dd9487f88764b19b15a170fd6
pzawadzki <admin@machinacyfrowa.pl>
2023-03-13 12:22:50 +0100

    Dodaj pliki projektów.

---
+++ SimpleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿//zadeklaruje sobie zmienną "a"
+int a;
+
+//inicjuję zmiennną "a"
+a = 5;
+
+//deklaracja z inicjalizacją
+int b = 6;
+
+//zapisz sumę a i b do zmiennej suma
+int suma = a + b;
+
+//wypisz na ekranie
+Console.WriteLine("Suma wyrazów a i b wynosi: " + suma.ToString());
