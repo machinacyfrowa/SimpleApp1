cbed0772891edbc6f09b418c645534766ba4d55a
pzawadzki <admin@machinacyfrowa.pl>
2023-03-13 12:32:34 +0100

    proste dodawanie liczb użytkownika

--- SimpleApp1/Program.cs
+++ SimpleApp1/Program.cs
@@ -11,4 +11,22 @@ int b = 6;
 int suma = a + b;
 
 //wypisz na ekranie
+//Console.WriteLine("Suma wyrazów a i b wynosi: " + suma.ToString());
+
+//Pobierz liczbę a od użytkownika
+Console.WriteLine("Podaj proszę liczbę a: ");
+//zapisz do zmiennej odpowiedz (deklaracja bo nie mieliśmy jej wcześniej)
+string odpowiedz = Console.ReadLine();
+//skonwertuj łańcuch tekstowy na liczbę
+a = int.Parse(odpowiedz);
+//osbierz od użytkownika liczbę b
+Console.WriteLine("Podaj proszę liczbę b: ");
+//zapisujemy do stringa ale bez deklaracji bo zmiena juz instnieje
+odpowiedz = Console.ReadLine();
+//konwertujemy na liczbę
+b = int.Parse(odpowiedz);   
+
+//sumujemy wartość zmiennych
+suma = a + b;
+//wypisujemy na ekranie odpowiedź
 Console.WriteLine("Suma wyrazów a i b wynosi: " + suma.ToString());
